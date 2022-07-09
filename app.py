1bf229b2a23d458fc64af757f4909b2d07b31511
mvladlena85 <mvladlena@yandex.ru>
2022-07-10 01:13:03 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import utils
+
+app = Flask(__name__)
+
+path = "candidates.json"
+candidates_list = utils.load_candidates(path)
+
+
+@app.route("/")
+def page_index():
+    return f"<pre>{utils.get_all(candidates_list)}</pre>"
+
+
+@app.route("/candidates/<int:x>/")
+def page_candidate(x):
+    candidate_data = utils.get_by_pk(candidates_list, x)
+    url = candidate_data[0]
+    print(url)
+    return f"<img src='{url}'>" \
+           f"<pre>{candidate_data[1]}</pre>"
+
+
+@app.route("/skills/<x>")
+def page_skilled_candidates(x):
+    return f"<pre>{utils.get_by_skill(candidates_list, x)}</pre>"
+
+
+app.run()
---
+++ utils.py
@@ -0,0 +1,44 @@
+import json
+
+
+def load_candidates(path: str):  # , которая загрузит данные из файла
+    with open(path, "r", encoding="utf-8") as f:
+        json_data = json.load(f)
+        return json_data
+
+
+def get_all(candidates: list):  # которая покажет всех кандидатов
+    candidate_data = ""
+    for candidate in candidates:
+        candidate_data += f'Имя кандидата - {candidate["name"]}\n' \
+                          f'Позиция кандидата - {candidate["position"]}\n' \
+                          f'Навыки - {candidate["skills"]}\n\n'
+    return candidate_data
+
+
+def get_by_pk(candidates: list, pk: int):  # которая вернет кандидата по pk
+    for candidate in candidates:
+        if candidate["pk"] == pk:
+            return candidate["picture"], f'Имя кандидата - {candidate["name"]}\n' \
+                    f'Позиция кандидата - {candidate["position"]}\n' \
+                    f'Навыки - {candidate["skills"]}\n'
+    return "Неверно указан pk"
+
+
+def get_by_skill(candidates: list, skill_name: str):  # , которая вернет кандидатов по навыку
+    candidate_data = ""
+    for candidate in candidates:
+        if skill_name.lower() in candidate["skills"].lower():
+            candidate_data += f'Имя кандидата - {candidate["name"]}\n' \
+                              f'Позиция кандидата - {candidate["position"]}\n' \
+                              f'Навыки - {candidate["skills"]}\n\n'
+    return candidate_data
+
+
+candidate_list = load_candidates("candidates.json")
+# print(get_all(candidate_list))
+# print(get_by_pk(candidate_list, 1))
+cand = get_by_pk(candidate_list, 4)
+print(cand[0])
+print(cand[1])
+# print(get_by_skill(candidate_list, "django"))
